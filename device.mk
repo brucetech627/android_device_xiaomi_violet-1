fdf35fa11ec05da2255857eab626549d122ebbd5
Rohan Purohit <purohit.rohan@gmail.com>
2020-05-05 12:44:43 +0530

    Violet: Switched to Custom Dose Overlay.

--- device.mk
+++ device.mk
@@ -19,6 +19,10 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/violet/violet-vendor.mk)
 
+# Doze
+PRODUCT_PACKAGES += \
+    CustomDoze
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
